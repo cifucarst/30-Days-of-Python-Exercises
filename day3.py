cc2651af92511adb6ddd31d127bdcd04363479f8
Carlos Cifuentes <cifentesparraandres@gmail.com>
2023-05-28 20:26:33 -0500

    fixed a little problem

--- day3.py
+++ day3.py
@@ -12,5 +12,10 @@ if __name__ == '__main__':
     N = int(input().strip())
     if N % 2 != 0:
         print("Weird")
-    elif N % 2 == 0 and N >= 20:
-        print("Not Weird")        
+    else:
+        if N >= 2 and N <= 5:
+            print("Not Weird")
+        elif N >= 6 and N <= 20:
+            print("Weird")
+        elif N > 0:
+            print("Not Weird")       
