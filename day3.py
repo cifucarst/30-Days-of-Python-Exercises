92bae7a53172e962d3e5b035e2d2ec7b61faf4c5
Carlos Cifuentes <cifentesparraandres@gmail.com>
2023-05-28 20:11:35 -0500

    day three

---
+++ day3.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 != 0:
+        print("Weird")
+    elif N % 2 == 0 and N >= 20:
+        print("Not Weird")        
